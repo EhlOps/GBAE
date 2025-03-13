1b90885f8e6290925d73f1f50a5a4a9de65b6b9b
EhlOps <samtehlers@gmail.com>
2025-03-13 15:55:58 -0400

    Cleanup and docs

--- lib/ram.c
+++ lib/ram.c
@@ -2,6 +2,12 @@
 
 static ram_context ctx;
 
+/**
+ * @brief Reads the two bytes at the given address in WRAM.
+ * 
+ * @param addr The address to read from.
+ * @return uint16_t The two bytes at the address.
+ */
 uint16_t read_wram(uint32_t addr) {
     addr -= WRAM_START;
 
@@ -13,6 +19,12 @@ uint16_t read_wram(uint32_t addr) {
     return ctx.wram[addr];
 }
 
+/**
+ * @brief Writes two bytes to WRAM.
+ * 
+ * @param addr The address to write to.
+ * @param val The value to write.
+ */
 void write_wram(uint32_t addr, uint16_t val) {
     addr -= WRAM_START;
 
@@ -24,6 +36,33 @@ void write_wram(uint32_t addr, uint16_t val) {
     ctx.wram[addr] = val;
 }
 
+/**
+ * @brief Reads the four bytes at the given address in WRAM.
+ * 
+ * @param addr The address to read from.
+ * @return uint32_t The four bytes at the address.
+ */
+uint32_t read_wram32(uint32_t addr) {
+    return (read_wram(addr) | (read_wram(addr + 2) << 16));
+}
+
+/**
+ * @brief Writes four bytes to WRAM.
+ * 
+ * @param addr The address to write to.
+ * @param val The value to write.
+ */
+void write_wram32(uint32_t addr, uint32_t val) {
+    write_wram(addr, val & 0xFFFF);
+    write_wram(addr + 2, val >> 16);
+}
+
+/**
+ * @brief Reads the four bytes at the given address in HRAM.
+ * 
+ * @param addr The address to read from.
+ * @return uint32_t The four bytes at the address.
+ */
 uint32_t read_hram(uint32_t addr) {
     addr -= HRAM_START;
 
@@ -35,6 +74,12 @@ uint32_t read_hram(uint32_t addr) {
     return ctx.hram[addr];
 }
 
+/**
+ * @brief Writes four bytes to HRAM.
+ * 
+ * @param addr The address to write to.
+ * @param val The value to write.
+ */
 void write_hram(uint32_t addr, uint16_t val) {
     addr -= HRAM_START;
 
